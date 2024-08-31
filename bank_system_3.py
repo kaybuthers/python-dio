067d8826b4646dc3c87c108c902a0a98f2c208aa
Erika <79380106+kaybuthers@users.noreply.github.com>
2024-08-31 17:48:13 -0300

    feat: update bank_system_3.py

--- bank_system_3.py
+++ bank_system_3.py
@@ -198,7 +198,7 @@ def recuperar_conta_cliente(cliente):
         print("\n@@@ Cliente não possui conta! @@@")
         return
 
-    # FIXME: não permite cliente escolher a conta
+    
     return cliente.contas[0]
 
 
